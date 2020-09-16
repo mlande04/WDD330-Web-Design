b2468d7fbd5041df79e9ff36c3333bb9257b6308
Michelle Landen <lan18002@byui.edu>
2020-09-16 16:20:16 -0700

    Create main.js
    Portfolio table of contents

---
+++ js/main.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+      <title>WDD 330 Portfolio</title>
+   </head>
+
+   <body>
+      const links = [
+        {
+          label: "Week1 notes",
+          url: "week1/index.html"
+        }
+      ]
+      
+      <menu>
+         <li><a href="week1.html"></li>
+      </menu>
+      
+   </body>
+
+</html>
