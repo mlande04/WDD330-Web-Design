83efa9fe69b85434b0115d9f5e63fdf5ac2e7c9f
Michelle Landen <lan18002@byui.edu>
2020-09-21 16:25:54 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-<!DOCTYPE html>
-<html>
-
-   <head>
-      <title>WDD 330 Portfolio</title>
-   </head>
-
-   <body>
-      const links = [
-        {
-          label: "Week1 notes",
-          url: "week1/index.html"
-        }
-      ]
+const links = [
+   {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+]
       
-      <menu>
-         <li><a href="week1.html"></li>
-      </menu>
-      
-   </body>
-
-</html>
+const ol = document.getElementById('assignments');      // whatever your OL tag ID is
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+  // add other tags of interest if you like (date submitted, score)
+  li.appendChild(a);
+  ol.appendChild(li);
+});
