5867944eec6618c8e5f77bac9f5fc3b9b9805d4e
Michelle Landen <lan18002@byui.edu>
2020-10-01 12:26:09 -0700

    Create main.js

---
+++ week3/events/main.js
@@ -0,0 +1,9 @@
+//function doSomething(event){
+//    console.log(event.target);
+//}
+//addEventListener('click', doSomething);
+
+const clickParagraph = document.getElementById('click');
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
