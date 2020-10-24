3b78fcd2f8185c2303e1cbb64b7e662c1cc4a9ec
Michelle Landen <lan18002@byui.edu>
2020-10-24 15:39:37 -0700

    Update main.js

--- Week6/toDoList/main.js
+++ Week6/toDoList/main.js
@@ -1,4 +1,21 @@
-// Create a "close" button to permanently delete items and append it to each list item
+/****************** Actions ******************/
+
+const ul = document.querySelector("ul");
+//const button = document.querySelector("button");
+const tasks = document.getElementById("input");
+let itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+let checkedItems = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+
+const toDoTask = JSON.parse(localStorage.getItem("items"));
+
+/**
+ * Create a "close" button
+ * Permanently delete items and append it to each list item
+ */
 var myList = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myList.length; i++) {
@@ -16,16 +33,23 @@ for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+/** Add a "checked" symbol when clicking on a list item
+ * Marks a list item as "done"
+ */
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function(ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+      checkedItems.push();
+    }
+  },
+  false
+);
 
 // Create a new list item when clicking on the "Add" button
 function newItem() {
@@ -33,7 +57,7 @@ function newItem() {
   var inputValue = document.getElementById("input").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("Please input a task item for the to-do list!");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -50,6 +74,38 @@ function newItem() {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
+  itemsArray.push(inputValue);
+  localStorage.setItems("items", JSON.stringify(itemsArray));
 }
+
+/**
+ * Show all to-do items when clicking on the "All" button
+ */
+var inputValue = document.getElementById("input").value;
+
+var allBtn = document.getElementById("all");
+allBtn.addEventListener("click", function showAll() {
+  inputValue.get();
+  inputValue.style.display = "block";
+});
+
+var doneBtn = document.getElementById("done");
+doneBtn.addEventListener("click", function showDone() {
+  checkedItems.style.display = "block";
+});
+
+var toDoBtn = document.getElementById("toDo");
+toDoBtn.addEventListener("click", function showToDo() {
+  itemsArray.pop(checkedItems);
+  itemsArray.style.display = "block";
+});
+
+var clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", function clearAll() {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+});
