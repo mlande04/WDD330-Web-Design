879274cf8894bfd4cab0c626947a538e17ce7273
Michelle Landen <lan18002@byui.edu>
2020-11-11 14:35:26 -0800

    Update main.js
    added more modern code using "requestAnimationFrame()"

--- week9/animation/main.js
+++ week9/animation/main.js
@@ -1,6 +1,19 @@
+// using setInterval
+/*
 const squareElement = document.getElementById('square');
 let angle = 0;
 setInterval( () => {
     angle = (angle + 2) % 360;
     squareElement.style.transform = `rotate(${angle}deg)`
 }, 1000/60);
+*/
+
+// using requestAnimationFrame
+const squareElement = document.getElementById('square');
+let angle = 0;
+function rotate() {
+    angle = (angle + 2)%360;
+    squareElement.style.transform = `rotate(${angle}deg)`
+    window.requestAnimationFrame(rotate);
+}
+const id = requestAnimationFrame(rotate);
