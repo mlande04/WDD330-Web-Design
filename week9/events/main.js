74411bd7b8f0c78aae80c2009bc0a079997167be
Michelle Landen <lan18002@byui.edu>
2020-11-11 15:30:48 -0800

    Update main.js

--- week9/events/main.js
+++ week9/events/main.js
@@ -12,27 +12,41 @@ btn.addEventListener('click', change);
 
 const form = document.forms[0];
 form.addEventListener('submit', factorize, false);
+//function factorize(event) {
+    // prevent the form from being submitted
+   // event.preventDefault();     
+    //const number = Number(form.number.value);
+    //document.getElementById('output').innerText = factorsOf(number);  
+//}
 function factorize(event) {
     // prevent the form from being submitted
-    event.preventDefault();     
+    event.preventDefault();   
+    document.getElementById('output').innerHTML = '<p>This could take a while ...</p>';
     const number = Number(form.number.value);
-    document.getElementById('output').innerText = factorsOf(number);  
+    if(window.Worker) {
+        const worker = new Worker('factors.js');
+        worker.postMessage(number);
+        worker.addEventListener('message', (event) => {
+        document.getElementById('output').innerHTML = event.data;
+        }, false);
+    }
 }
-function factorsOf(n) {
-    if(Number.isNaN(Number(n))) {
-        throw new RangeError('Argument Error: Value must be an integer');
-    }
-    if(n < 0) {
-        throw new RangeError('Argument Error: Number must be positive');
-    }
-    if(!Number.isInteger(n)) {
-        throw new RangeError('Argument Error: Number must be an integer');
-    }
-    const factors = [];
-    for (let i=1 , max = Math.sqrt(n); i <= max ; i++) {
-        if (n%i === 0){
-        factors.push(i,n/i);
-        }
-    }
-    return factors.sort((a,b) =>  a - b);
-}
+
+//function factorsOf(n) {
+//    if(Number.isNaN(Number(n))) {
+//        throw new RangeError('Argument Error: Value must be an integer');
+//    }
+//    if(n < 0) {
+//        throw new RangeError('Argument Error: Number must be positive');
+//    }
+//   if(!Number.isInteger(n)) {
+//        throw new RangeError('Argument Error: Number must be an integer');
+//    }
+//    const factors = [];
+//    for (let i=1 , max = Math.sqrt(n); i <= max ; i++) {
+//        if (n%i === 0){
+//        factors.push(i,n/i);
+//        }
+//    }
+//    return factors.sort((a,b) =>  a - b);
+//}
